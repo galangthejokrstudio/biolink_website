1d3a807725d1b241b8369c9ad9d37b5b7ff88d07
ilylucas <lucasribeiroo2010@gmail.com>
2024-10-11 16:24:09 -0300

    back up

--- src/components/LinksGridButton.jsx
+++ src/components/LinksGridButton.jsx
@@ -1,20 +1,12 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { analytics } from '@vercel/analytics';  // Ensure Vercel Analytics is properly set up
 
 const LinksGridButton = ({ children, text, link }) => {
-  
-  const handleClick = () => {
-    // Track a custom event with Vercel Analytics
-    analytics.track('Button Clicked', { text }); // 'text' is passed as metadata
-  };
 
   return (
     <a 
       target='_blank' 
       className="bg-black/20 p-4 rounded-lg backdrop-blur-lg ease-in-out duration-300 hover:bg-black/40 flex items-center shadow-lg shadow-black/20" 
       href={link}
-      onClick={handleClick} // Trigger the custom event on click
     >
       {children}
       {text}
